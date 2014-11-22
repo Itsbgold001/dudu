17ff7364f52e3177c4112c66f4798e8a81e4311a
malcolm maima <malcolmmaima@gmail.com>
2014-11-22 20:34:35 +0300

    Update track.php
    Place tracking code for web analytics

--- track.php
+++ track.php
@@ -1,4 +1,3 @@
-<a title="Web Analytics" href="http://clicky.com/100738079"></a>
-<script src="//static.getclicky.com/js" type="text/javascript"></script>
-<script type="text/javascript">try{ clicky.init(100738079); }catch(e){}</script>
-<noscript><p></p></noscript>
+/*
+* Place tracking code here
+*/
