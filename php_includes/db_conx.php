21d6c1aad1d6ca3313532a290ae82e6abacf65a4
malcolm maima <malcolmmaima@gmail.com>
2014-11-22 20:37:53 +0300

    Update db_conx.php
    Dudu main Mysql connection script.
    /* Set connection configurations*/

--- php_includes/db_conx.php
+++ php_includes/db_conx.php
@@ -1,5 +1,5 @@
 <?php
-$db_conx = mysqli_connect("localhost","root","","dudu");
+$db_conx = mysqli_connect("localhost","db_user","db_password","db_name");
 if(mysqli_connect_errno()){
 	echo mysqli_connect_error();
 	exit();
